b75d9bf8320418ade5186f23f381a3c90fedad3a
Josh Hall <joshhall@Joshs-MacBook-Pro.local>
2020-03-09 19:59:32 -0400

    removed order service component

--- gr-recycle/src/app/app.module.ts
+++ gr-recycle/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecycleItemComponent } from './components/recycle-item/recycle-item.component';
-import { OrderServiceComponent } from './components/order-service/order-service.component';
 import { LocationsComponent } from './components/locations/locations.component';
 import { ResourcesComponent } from './components/resources/resources.component';
 import { ReportMissingComponent } from './components/report-missing/report-missing.component';
@@ -13,7 +12,6 @@ import { ReportMissingComponent } from './components/report-missing/report-missi
   declarations: [
     AppComponent,
     RecycleItemComponent,
-    OrderServiceComponent,
     LocationsComponent,
     ResourcesComponent,
     ReportMissingComponent
