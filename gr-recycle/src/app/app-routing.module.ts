161ad11761e020558e7a0cf0543a4d1a64f2e533
Josh Hall <joshhall@Joshs-MacBook-Pro.local>
2020-03-10 14:17:35 -0400

    set up a few routes

--- gr-recycle/src/app/app-routing.module.ts
+++ gr-recycle/src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LocationsComponent } from './locations/locations.component';
+import { ReportMissingComponent } from './report-missing/report-missing.component';
+import { RecycleItemComponent } from './recycle-item/recycle-item.component';
+import { ResourcesComponent } from './resources/resources.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reportmissing',
+    component: ReportMissingComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'recycleitems',
+    component: RecycleItemComponent
+  },
+  {
+    path: 'resources',
+    component: ResourcesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, 
+  LocationsComponent, ReportMissingComponent, 
+  RecycleItemComponent, ResourcesComponent];
--- gr-recycle/src/app/app.module.ts
+++ gr-recycle/src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { RecycleItemComponent } from './recycle-item/recycle-item.component';
-import { ResourcesComponent } from './resources/resources.component';
-import { ReportMissingComponent } from './report-missing/report-missing.component';
+// import { RecycleItemComponent } from './recycle-item/recycle-item.component';
+// import { ResourcesComponent } from './resources/resources.component';
+// import { ReportMissingComponent } from './report-missing/report-missing.component';
 import { FindRouteComponent } from './find-route/find-route.component';
-import { LocationsComponent } from './locations/locations.component';
+// import { LocationsComponent } from './locations/locations.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecycleItemComponent,
-    ResourcesComponent,
-    ReportMissingComponent,
+    routingComponents,
+    // RecycleItemComponent,
+    // ResourcesComponent,
     FindRouteComponent,
-    LocationsComponent
+    // LocationsComponent
   ],
   imports: [
     BrowserModule,
