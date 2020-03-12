f3a21262ce77e38efa574a6c019d121865a71b7e
Josh Hall <joshhall@Joshs-MacBook-Pro.local>
2020-03-12 15:43:39 -0400

    removed comments

--- gr-recycle/src/app/app.module.ts
+++ gr-recycle/src/app/app.module.ts
@@ -2,14 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module'; 
-//routingComponents holds all components declared in routing module
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { FindRouteComponent } from './find-route/find-route.component';
 import { LocationsComponent } from './locations/locations.component';
-// import { DatePipePipe } from './date-pipe.pipe';
-// import { DatePipe } from './date.pipe';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { HttpClientModule } from '@angular/common/http'
 
@@ -19,9 +16,7 @@ import { HttpClientModule } from '@angular/common/http'
     AppComponent,
     routingComponents,
     FindRouteComponent,
-    LocationsComponent,
-    // DatePipePipe,
-    // DatePipe
+    LocationsComponent
   ],
   imports: [
     BrowserModule,
