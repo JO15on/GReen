4a5a9c33a95fc7640f779d3615bc9637263132b7
Rachael <rachaelvruggink@gmail.com>
2020-03-11 21:25:45 -0400

    starting the recycle item component

--- gr-recycle/src/app/app.module.ts
+++ gr-recycle/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule, routingComponents } from './app-routing.module'; 
-//routingComponents holds all components declared in routing module
-
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { FindRouteComponent } from './find-route/find-route.component';
 import { LocationsComponent } from './locations/locations.component';
-// import { DatePipePipe } from './date-pipe.pipe';
-// import { DatePipe } from './date.pipe';
+
 
 
 @NgModule({
@@ -18,8 +14,6 @@ import { LocationsComponent } from './locations/locations.component';
     routingComponents,
     FindRouteComponent,
     LocationsComponent,
-    // DatePipePipe,
-    // DatePipe
   ],
   imports: [
     BrowserModule,
