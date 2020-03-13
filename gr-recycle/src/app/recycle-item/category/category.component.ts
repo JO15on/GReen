f52192ffcc814524d9b0506cdc540b52b3ad1170
Rachael <rachaelvruggink@gmail.com>
2020-03-12 20:13:20 -0400

    category stuff

--- gr-recycle/src/app/recycle-item/category/category.component.ts
+++ gr-recycle/src/app/recycle-item/category/category.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'], 
+
 })
 export class CategoryComponent implements OnInit {
+  show: boolean = false;
+
 
   constructor() { }
 
--- gr-recycle/src/app/recycle-item/recycle-item.component.ts
+++ gr-recycle/src/app/recycle-item/recycle-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recycle-item',
   templateUrl: './recycle-item.component.html',
-  styleUrls: ['./recycle-item.component.css']
+  styleUrls: ['./recycle-item.component.css'],
+
 })
 export class RecycleItemComponent implements OnInit {
 
