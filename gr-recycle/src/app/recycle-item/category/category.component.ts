b2926a69e7988baaabd98fd3316f07dd4c02a1e6
Josh Hall <joshhall@Joshs-MBP.attlocal.net>
2020-03-15 15:20:18 -0400

    updated styling?

--- gr-recycle/src/app/recycle-item/category/category.component.ts
+++ gr-recycle/src/app/recycle-item/category/category.component.ts
@@ -22,7 +22,8 @@ export class CategoryComponent implements OnInit {
       },
     ],  
   },
-  {"holiday": [
+  { 
+    "holiday": [
     {
       itemName: "TREE",
       description: "Automotive batteries may be recycled when you purchase a new car battery.  If you are not purchasing a car battery but have scrap batteries you would like to dispose of, please take it to one of the following businesses for recycling.",
@@ -36,23 +37,17 @@ export class CategoryComponent implements OnInit {
   ],}
   ];
 
-// values: { automotive: { itemName: string; description: string; dropOffLocations: string; }[]; }; 
 values :any;
 category : string;
 
   catSelected(catIndex : string){
    const index = this.categories.findIndex(category => {
-    console.log(typeof Object.keys(category)[0]);
-    console.log(typeof catIndex);
       return Object.keys(category)[0].trim() == catIndex;
       
     })
-    console.log(index);
     this.values = this.categories[index];
     this.category = catIndex;
-    console.log(this.values);
     return this.values;
-
   }
 
   constructor() { }
@@ -63,4 +58,3 @@ category : string;
 }
 
 
-// this.values = {data : this.categories[index], category: catIndex };
