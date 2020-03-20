91bf546f573068097b23e51f6e6ceb9894c47c48
Bryce <bykerbry@umich.edu>
2020-03-19 23:31:56 -0400

    adjusted path to nav icons

--- src/app/home/feed.service.ts
+++ src/app/home/feed.service.ts
@@ -8,22 +8,22 @@ export class FeedService {
   notification: Inotification[] = [
    
     {
-      icon: "/assets/news_icon.png",
+      icon: "assets/news_icon.png",
       headline: "Reopening of the Kent County Recycling & Education Center!",
       datePosted: "Posted 3/27/2020",
       description: "New and improved recycling services for Grand Rapids residents. Visit our revamped location at 977 Wealthy Street SW.",
     },
     {
-      icon: "/assets/recycle_icon.png",
+      icon: "assets/recycle_icon.png",
       headline: "Recycling Update - No Longer Accepting Plastic Bags",
       datePosted: "Posted 3/27/2020",
       description: "Effective January 2020, plastic bags and other flexible plastics are no longer recyclable curbside in GR.",
     },
     {
-      icon: "/assets/calendar_icon.png",
+      icon: "assets/calendar_icon.png",
       headline: "Easter Holiday Collection Schedule Update",
       datePosted: "Posted 3/27/2020",
-      description: "Refuse and recycling pick-ups will be pushed back by a day to accomodate the holiday schedule.",
+      description: "Refuse and recycling pick-ups will be pushed back by a day to accommodate the holiday schedule.",
     },
   ]
 
