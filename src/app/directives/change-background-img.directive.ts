eeced3cf0a427010352c63fa54025f98a387a34a
Bryce <bykerbry@umich.edu>
2020-03-20 21:26:25 -0400

    minor refactoring/rmv old commented code

--- src/app/directives/change-background-img.directive.ts
+++ src/app/directives/change-background-img.directive.ts
@@ -8,15 +8,14 @@ export class ChangeBackgroundImgDirective {
   constructor() { }
 
   @HostListener('focus', ['$event.target'])
-  @HostListener('mouseenter', ['$event.target']) onMouseOver(e: any) {
-    e.style.backgroundImage = `url('assets/${e.id}_icon_hover.png')`
+  @HostListener('mouseenter', ['$event.target']) onMouseOver(btn: any) {
+    btn.style.backgroundImage = `url('assets/${btn.id}_icon_hover.png')`
   }
 
   @HostListener('blur', ['$event.target'])
-  @HostListener('mouseleave', ['$event.target']) onMouseLeave(e: any) {
-    if (document.activeElement !== e) {
-      e.style.backgroundImage = `url('assets/${e.id}_icon.png')`
+  @HostListener('mouseleave', ['$event.target']) onMouseLeave(btn: any) {
+    if (document.activeElement !== btn) {
+      btn.style.backgroundImage = `url('assets/${btn.id}_icon.png')`
     }
   }
-
 }
