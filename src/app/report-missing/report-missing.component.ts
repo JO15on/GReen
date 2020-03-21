6049e2fdbc3dd3fdd495a61f8208aed2975798a8
JO15on <JOlson1530@hotmail.com>
2020-03-21 14:45:12 -0400

    required form is working!

--- src/app/report-missing/report-missing.component.ts
+++ src/app/report-missing/report-missing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-report-missing',
@@ -11,12 +12,24 @@ export class ReportMissingComponent implements OnInit {
   hidden : boolean = false;
   step: string = 'start';
 
-
+  reportForm : FormGroup;
   newDate = Date.now();
 
-  constructor() { }
+  constructor(private fb : FormBuilder) { 
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.reportForm = this.fb.group({
+      name: ['', Validators.required],
+      date: ['', Validators.required],
+      address: ['', Validators.required],
+      phone: ['', Validators.required],
+      email: ['', Validators.required]
+    });
   }
 
   yes(){
