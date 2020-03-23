02a184a5033f7a2c76deeec2272ab667f949dce1
Rachael <rachaelvruggink@gmail.com>
2020-03-22 23:08:07 -0400

    CSS updates, prepare for some merge conflicts

--- src/app/home/feed.service.ts
+++ src/app/home/feed.service.ts
@@ -15,7 +15,7 @@ export class FeedService {
     },
     {
       icon: "assets/recycle_icon.png",
-      headline: "Recycling Update - No Longer Accepting Plastic Bags",
+      headline: "No Longer Accepting Plastic Bags",
       datePosted: "Posted 3/27/2020",
       description: "Effective January 2020, plastic bags and other flexible plastics are no longer recyclable curbside in GR.",
     },
